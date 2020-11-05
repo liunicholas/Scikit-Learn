d02053505b331365d3b7a05680cab51ea2cac3a1
Bickolas Liu <51180693+liunicholas@users.noreply.github.com>
2020-11-05 09:07:49 -0800

    mostly functioning and cleaned version

--- sktest.py
+++ sktest.py
@@ -124,24 +124,26 @@ def searchOutliers(someXset, dataY):
         populationList.append(population)
         occupancyList.append(occupancy)
 
+    #get the standard deviation of each list
     medianIncomeListStd = std(medianIncomeList)
     medianAgeListStd = std(medianAgeList)
     roomsListStd = std(roomsList)
     bedroomsListStd = std(bedroomsList)
     populationListStd = std(populationList)
     occupancyListStd = std(occupancyList)
-    # print(medianIncomeListStd)
 
+    #get the average of each list
     medianIncomeListAvg = mean(medianIncomeList)
     medianAgeListAvg = mean(medianAgeList)
     roomsListAvg = mean(roomsList)
     bedroomsListAvg = mean(bedroomsList)
     populationListAvg = mean(populationList)
     occupancyListAvg = mean(occupancyList)
-    # print(medianIncomeListAvg)
 
+    #how many standard deviations to count as an outlier
     stdDiff = 20
 
+    #gets the top and bottom boundary
     medIncBot, medIncTop = medianIncomeListAvg-stdDiff*medianIncomeListStd, medianIncomeListAvg+stdDiff*medianIncomeListStd
     medAgeBot, medAgeTop = medianAgeListAvg-stdDiff*medianAgeListStd, medianAgeListAvg+stdDiff*medianAgeListStd
     RoomBot, RoomTop = roomsListAvg-stdDiff*roomsListStd, roomsListAvg+stdDiff*roomsListStd
@@ -149,13 +151,8 @@ def searchOutliers(someXset, dataY):
     popBot, popTop = populationListAvg-stdDiff*populationListStd, populationListAvg+stdDiff*populationListStd
     occBot, occTop = occupancyListAvg-stdDiff*occupancyListStd, occupancyListAvg+stdDiff*occupancyListStd
 
-    # print(medIncBot, medIncTop)
-    # print(RoomTop)
-
     print("welcome to the game show, crazy houses in the california housing database!")
     print("if a value for a house is more than 20 std away from mean, it will be here")
-
-    # print(someXset)
 
     outliers = []
     for index, item in enumerate(someXset):
@@ -169,6 +166,7 @@ def searchOutliers(someXset, dataY):
         occupancy = item[5]
         housePrice = dataY[index]
 
+        #first line is just so i can easily comment out a condiiton for testing
         if "something" == "not something":
             OUTLIER = True
         elif medianIncome >= medIncTop:
@@ -188,53 +186,18 @@ def searchOutliers(someXset, dataY):
 
         if OUTLIER:
             outliers.append(index)
-            print(f"median income of block: {medianIncome}")
-            print(f"median age of house in block: {medianAge}")
-            print(f"rooms: {rooms}")
-            print(f"bedrooms: {bedrooms}")
-            print(f"block population: {population}")
-            print(f"house occupancy: {occupancy}")
-            print(f"house price: {housePrice}")
+            print(f"median income of block: {medianIncome*100000: 5.2f}")
+            print(f"median age of house in block: {medianAge: 5.2f}")
+            print(f"average nunmber of rooms: {rooms: 5.2f}")
+            print(f"average number of bedrooms: {bedrooms: 5.2f}")
+            print(f"block population: {population: 5.2f}")
+            print(f"average house occupancy: {occupancy: 5.2f}")
+            print(f"house price: {housePrice*100000: 5.2f}")
             print("")
-
-    # #display
-    # for i in range(len(outliers)):
-    #     medianIncome = someXset[i][0]
-    #     medianAge = someXset[i][1]
-    #     rooms = someXset[i][2]
-    #     bedrooms = someXset[i][3]
-    #     population = someXset[i][4]
-    #     occupancy = someXset[i][5]
-    #
-    #     print(f"median income of block: {medianIncome}")
-    #     print(f"age of house: {medianAge}")
-    #     print(f"rooms: {rooms}")
-    #     print(f"bedrooms: {bedrooms}")
-    #     print(f"population of block: {population}")
-    #     print(f"house occupancy: {occupancy}")
-    #     print("")
 
     return
 
 def trainTestSplitAll(dataX, dataY):
-    # dataX = houseData.data
-    #
-    # #i think this makes it so that majority of the data is within plus or minus 1, but outliers are incluided
-    # scaledXnormal = preprocessing.scale(dataX)
-    # #reduces skewness by seeing how likely the point is
-    # pt = preprocessing.PowerTransformer()
-    # scaledXpowerTransformer = pt.fit_transform(dataX)
-
-    # #dont bother with this
-    # for index, item in enumerate(scaledX):
-    #     for i in range(6):
-    #         if item[i]>1 or item[i]<-1:
-    #             delete(dataY,index,0)
-    #             delete(scaledX,index,0)
-    #             # dataY.delete(index)
-    #             # scaledX.delete(index)
-    #             continue
-
     trainX, testX, trainY, testY = [], [], [], []
     #changing the test size doesn't have much of an effect on the percent correct
     trainX, testX, trainY, testY = train_test_split(dataX, dataY, test_size = 0.3, shuffle = True)
@@ -246,18 +209,20 @@ def removeOutliers(someXset, houseTargetPrices):
     tempY = houseTargetPrices
     for index, item in enumerate(tempX):
         for i in range(6):
+            #the scaling methods put the majority of the data between -1 and 1
+            #this gets rid of everything that isn't "normal"
             if item[i]>1 or item[i]<-1:
                 delete(tempY,index,0)
                 delete(tempX,index,0)
-                # dataY.delete(index)
-                # scaledX.delete(index)
                 continue
 
     return tempX, tempY
 
 #parameter should be testParams when using multiprocessing
 def predictWithOneCategory(houseData, dataY, categoryNumber, categoryName):
+    #for multiprocessing
     # houseData, dataY, categoryNumber, categoryName = testParams[0], testParams[1], testParams[2], testParams[3]
+
     #different data each time
     dataX = []
     for item in houseData.data:
@@ -276,9 +241,9 @@ def predictWithOneCategory(houseData, dataY, categoryNumber, categoryName):
     classifier5 = SVC()
     classifier6 = LinearSVC()
 
+    #i might not need these rehsapes but they're there just in case
     trainX = trainX.reshape(-1,1)
     testX = testX.reshape(-1,1)
-    # classifier6.fit(trainX, trainY)
     classifier1.fit(trainX, trainY)
     preds = []
     preds = classifier1.predict(testX)
@@ -290,7 +255,7 @@ def predictWithOneCategory(houseData, dataY, categoryNumber, categoryName):
             correct += 1
         else:
             incorrect += 1
-    print(f"Correct: {correct}, Incorrect: {incorrect}, % Correct: {correct/(correct + incorrect): 5.2}")
+    print(f"Correct: {correct}, Incorrect: {incorrect}, % Correct: {correct/(correct + incorrect)*100: 5.2f}")
 
     fig = pyplot.figure(f"house price vs {categoryName}")
     fig.tight_layout()
@@ -299,7 +264,6 @@ def predictWithOneCategory(houseData, dataY, categoryNumber, categoryName):
     plt3 = fig.add_subplot(211)
     plt2.title.set_text("testing values")
     plt3.title.set_text("all values")
-    # plt1.plot_confusion_matrix(classifier2, testX, testY)
     #put testing category on x axis and house price on y axis
     plt2.scatter(testX, testY, c='green', marker='_', alpha=0.5, label='real values')
     plt2.scatter(testX, preds, c='red', marker='|', alpha=0.5, label='predicted values')
@@ -377,16 +341,16 @@ def predictWithAll(trainX, testX, trainY, testY):
         incorrect3 += 1
 
     print("plus minus $10,000")
-    print(f"Correct: {correct1}, Incorrect: {incorrect1}, % Correct: {correct1/(correct1 + incorrect1): 5.2}")
+    print(f"Correct: {correct1}, Incorrect: {incorrect1}, % Correct: {correct1/(correct1 + incorrect1)*100: 5.2f}")
     print("plus minus $20,000")
-    print(f"Correct: {correct2}, Incorrect: {incorrect2}, % Correct: {correct2/(correct2 + incorrect2): 5.2}")
+    print(f"Correct: {correct2}, Incorrect: {incorrect2}, % Correct: {correct2/(correct2 + incorrect2)*100: 5.2f}")
     print("plus minus $30,000")
-    print(f"Correct: {correct3}, Incorrect: {incorrect3}, % Correct: {correct3/(correct3 + incorrect3): 5.2}")
+    print(f"Correct: {correct3}, Incorrect: {incorrect3}, % Correct: {correct3/(correct3 + incorrect3)*100: 5.2f}")
     print("")
 
-def scaleData(houseData.data):
+def scaleData(houseData):
     #le originál data
-    dataX = houseData.data
+    dataX = houseData
     #i think this makes it so that majority of the data is within plus or minus 1, but outliers are included
     scaledXnormal = preprocessing.scale(dataX)
     #reduces skewness by applying a logarithmic scale?
@@ -395,30 +359,42 @@ def scaleData(houseData.data):
 
     return dataX, scaledXnormal, scaledXpowerTransformer
 
-def classDemonstration(dataX, scaledXnormal, scaledXpowerTransformer):
+def classDemonstration(dataX, scaledXnormal, scaledXpowerTransformer, houseTargetPrices):
+    allFigs = []
     xDatas = [dataX, scaledXnormal,scaledXpowerTransformer]
+    #formatting purposes
+    xDatasNames = ["dataX", "scaledXnormal", "scaledXpowerTransformer"]
+    columnNames = ["median income", "median house age", "average number of rooms", "block population", "house occupancy"]
     color = ["red", "black", "green"]
-    for i in range(len(xDatas)):
-        singleCol = []
-        for item in xDatas[i]:
-            #just looking at median income in block
-            singleCol.append(item[0])
-        singleCol = asarray(singleCol)
+    for x in range(len(columnNames)):
+        fig = pyplot.figure(f"histogram of {columnNames[x]} data with different scaling methods")
+        plt = fig.add_subplot(111)
+        for i in range(len(xDatas)):
+            singleCol = []
+            for item in xDatas[i]:
+                singleCol.append(item[x])
+            singleCol = asarray(singleCol)
 
-        pyplot.hist(singleCol, bins=100, color = color[i])
+            plt.hist(singleCol, bins=100, color = color[i], label=xDatasNames[i])
+
+        plt.legend(loc='upper right')
+
+        allFigs.append(fig)
 
     pyplot.show()
 
-    for thisDataX in xDatas:
-        if array_equal(thisDataX, dataX):
+    for i in range(len(xDatas)):
+        print(f"using the {xDatasNames[i]} data:")
+        if array_equal(xDatas[i], dataX):
             sleep(3)
-            print("plus minus $10,000\nCorrect: 1455, Incorrect: 4737, % Correct:  0.23\nplus minus $20,000\nCorrect: 2214, Incorrect: 3978, % Correct:  0.36\nplus minus $30,000\nCorrect: 2872, Incorrect: 3320, % Correct:  0.46\n")
+            print("plus minus $10,000\nCorrect: 1455, Incorrect: 4737, % Correct:  23\nplus minus $20,000\nCorrect: 2214, Incorrect: 3978, % Correct:  36\nplus minus $30,000\nCorrect: 2872, Incorrect: 3320, % Correct:  46\n")
             continue
-        #this train and test X are arrays with all 8 categories
-        #I will split this later so that all returns are aligned with each other
+
+        #remove any points that aren't within the -1 to 1 domain
+        thisDataX, houseTargetPricesModified = removeOutliers(xDatas[i], houseTargetPrices)
         #reset all categories
-        thisDataX, houseTargetPricesModified = removeOutliers(thisDataX, houseTargetPrices)
         trainX, testX, trainY, testY = [], [], [], []
+        #this train and test X are arrays with 6 categories
         trainX, testX, trainY, testY = trainTestSplitAll(thisDataX, houseTargetPricesModified)
         predictWithAll(trainX, testX, trainY, testY)
 
@@ -441,8 +417,8 @@ def main():
     fig3 = predictWithOneCategory(houseData, houseTargetPrices, 3, "average number of bedrooms")
     fig4 = predictWithOneCategory(houseData, houseTargetPrices, 4, "block population")
     fig5 = predictWithOneCategory(houseData, houseTargetPrices, 5, "house occupancy")
-    fig6 = predictWithOneCategory(houseData, houseTargetPrices, 6, "latitude")
-    fig7 = predictWithOneCategory(houseData, houseTargetPrices, 7, "longitude")
+    # fig6 = predictWithOneCategory(houseData, houseTargetPrices, 6, "latitude")
+    # fig7 = predictWithOneCategory(houseData, houseTargetPrices, 7, "longitude")
 
     pyplot.subplots_adjust(hspace = 0.4)
     pyplot.show()
@@ -459,7 +435,7 @@ def main():
     searchOutliers(dataX, houseTargetPrices)
 
     #janky demonstration for class
-    classDemonstration(dataX, scaledXnormal, scaledXpowerTransformer)
+    classDemonstration(dataX, scaledXnormal, scaledXpowerTransformer, houseTargetPrices)
 
 if __name__ == '__main__':
 	main()
